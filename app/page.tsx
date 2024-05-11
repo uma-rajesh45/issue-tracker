39c308ca58c63590a953d84515af55f13177071d
uma-rajesh45 <122291360+uma-rajesh45@users.noreply.github.com>
2024-05-11 08:29:19 +0530

    commit radix ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import { Button } from "@radix-ui/themes";
 
 export default function Home() {
   return (
-   <div><h1>hello world</h1></div>
+   <div><h1>hello world</h1>
+   <Button>hello</Button></div>
+   
   );
 }
