3f5445aa30a26f3c27e301bc909076aca5ec271f
uma-rajesh45 <122291360+uma-rajesh45@users.noreply.github.com>
2024-05-12 14:39:00 +0530

    handle the form errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,36 +1,56 @@
-'use client';
-import { Button, TextField } from "@radix-ui/themes";
+/* eslint-disable react-hooks/rules-of-hooks */
+"use client";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
-import { Controller, useForm,SubmitHandler} from "react-hook-form";
+import { Controller, useForm, SubmitHandler } from "react-hook-form";
 import { useRouter } from "next/navigation";
 import axios from "axios";
-import { routeModule } from "next/dist/build/templates/app-page";
+import { useState } from "react";
 
 interface FormData {
   title: string;
   description: string;
 }
 const page = () => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
   const navigate = useRouter();
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const { register,handleSubmit,control } = useForm<FormData>();
-  const onSubmit:SubmitHandler<FormData> =async (data)=>{
-    await axios.post("/api/issues",data);
-    navigate.push("/issues")
-  }
+  const [error, setError] = useState("");
+  const { register, handleSubmit, control } = useForm<FormData>();
+  const onSubmit: SubmitHandler<FormData> = async (data) => {
+    try {
+      await axios.post("/api/issues", data);
+      navigate.push("/issues");
+      
+    } catch (error) {
+      setError(" The minimum length of the title or description should be 1.")
+    }
+  };
   return (
-    <form className=" max-w-xl flex flex-col gap-3" onSubmit={handleSubmit(onSubmit)}>
-      <TextField.Root placeholder="Title" {...register("title")} />
-      <Controller
-      control={control}
-        name="description"
-        render={({ field }) => <SimpleMDE placeholder="Description" {...field}/>}
-      />
+    <div className=" max-w-xl">
+      {error && (
+        <Callout.Root className="mb-3" color="red">
+          <Callout.Text>
+           {error}
+          </Callout.Text>
+        </Callout.Root>
+      )}
 
-      <Button>Submit A New Isuue</Button>
-    </form>
+      <form
+        className=" max-w-xl flex flex-col gap-3"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <TextField.Root placeholder="Title" {...register("title")} />
+        <Controller
+          control={control}
+          name="description"
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+
+        <Button>Submit A New Isuue</Button>
+      </form>
+    </div>
   );
 };
 
