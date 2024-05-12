d30f92eef82a878e2a7fa07fcdba28425c3367e5
uma-rajesh45 <122291360+uma-rajesh45@users.noreply.github.com>
2024-05-12 08:58:50 +0530

    add a mark down editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,11 +1,11 @@
-import { Button, TextArea, TextField } from '@radix-ui/themes'
-import React from 'react'
-
+import { Button, TextField } from '@radix-ui/themes'
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 const page = () => {
   return (
     <div className=' max-w-xl flex flex-col gap-3'>
         <TextField.Root placeholder='Title'/>
-        <TextArea placeholder='description'/>
+        <SimpleMDE placeholder='description'/>
         <Button>Submit A New Isuue</Button>
     </div>
   )
