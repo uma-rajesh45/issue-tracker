d8ea50233a827dc07ac603f7c95e612cb3a06dcc
uma-rajesh45 <122291360+uma-rajesh45@users.noreply.github.com>
2024-05-12 11:45:34 +0530

    handling the form data

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,15 +1,37 @@
 'use client';
-import { Button, TextField } from '@radix-ui/themes'
+import { Button, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
-const page = () => {
-  return (
-    <div className=' max-w-xl flex flex-col gap-3'>
-        <TextField.Root placeholder='Title'/>
-        <SimpleMDE placeholder='description'/>
-        <Button>Submit A New Isuue</Button>
-    </div>
-  )
-}
+import { Controller, useForm,SubmitHandler} from "react-hook-form";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import { routeModule } from "next/dist/build/templates/app-page";
 
-export default page
+interface FormData {
+  title: string;
+  description: string;
+}
+const page = () => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const navigate = useRouter();
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { register,handleSubmit,control } = useForm<FormData>();
+  const onSubmit:SubmitHandler<FormData> =async (data)=>{
+    await axios.post("/api/issues",data);
+    navigate.push("/issues")
+  }
+  return (
+    <form className=" max-w-xl flex flex-col gap-3" onSubmit={handleSubmit(onSubmit)}>
+      <TextField.Root placeholder="Title" {...register("title")} />
+      <Controller
+      control={control}
+        name="description"
+        render={({ field }) => <SimpleMDE placeholder="Description" {...field}/>}
+      />
+
+      <Button>Submit A New Isuue</Button>
+    </form>
+  );
+};
+
+export default page;
