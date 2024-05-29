1f6b1392b4b7b5c1275967b3891c8dff9ad75f67
uma-rajesh45 <122291360+uma-rajesh45@users.noreply.github.com>
2024-05-29 15:12:46 +0530

    refactor the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -15,12 +15,6 @@ import React from "react";
 import { FaBug } from "react-icons/fa";
 
 const NavBar = () => {
-  const { status, data: session } = useSession();
-  const path = usePathname();
-  const links = [
-    { label: "Dashboard", href: "/" },
-    { label: "Issues", href: "/issues" },
-  ];
   return (
     <nav className="border-b mb-5 pb-5 pt-5">
       <Container>
@@ -31,55 +25,69 @@ const NavBar = () => {
                 <FaBug />
               </Link>
             </h3>
-            <ul className="flex gap-4">
-              {links.map((link) => (
-                <li key={link.href}>
-                  <Link
-                    href={link.href}
-                    className={classNames({
-                      "text-zinc-900": link.href === path,
-                      "text-zinc-500": link.href !== path,
-                      " hover:text-zinc-800 transition-colors": true,
-                    })}
-                  >
-                    {link.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+            <NavLinks/>
           </Flex>
-          <Box>
-            {status === "authenticated" && (
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Avatar
-                    src={session.user!.image!}
-                    fallback="?"
-                    size="2"
-                    radius="full"
-                    className="cursor-pointer"
-                    referrerPolicy="no-referrer"
-                  />
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content side="left">
-                  <DropdownMenu.Label>
-                    <Text size="2" weight="bold">
-                      {session.user!.email}
-                    </Text>
-                  </DropdownMenu.Label>
-                  <DropdownMenu.Item className="cursor-pointer">
-                    <Link href="/api/auth/signout">Logout</Link>
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-            )}
-            {status === "unauthenticated" && (
-              <Link href="/api/auth/signin">Login</Link>
-            )}
-          </Box>
+          <AuthStatus/>
         </Flex>
       </Container>
     </nav>
+  );
+};
+const NavLinks = () => {
+  const path = usePathname();
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+  return (
+    <ul className="flex gap-4">
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            href={link.href}
+            className={classNames({
+              'nav-link':true,
+              "!text-zinc-900": link.href === path,
+            })}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+const AuthStatus = () => {
+  const { status, data: session } = useSession();
+  if (status === "loading") return null;
+  if (status === "unauthenticated")
+    return <Link href="/api/auth/signin" className="nav-link">Login</Link>;
+
+  return (
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            src={session!.user!.image!}
+            fallback="?"
+            size="2"
+            radius="full"
+            className="cursor-pointer"
+            referrerPolicy="no-referrer"
+          />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content side="left">
+          <DropdownMenu.Label>
+            <Text size="2" weight="bold">
+              {session!.user!.email}
+            </Text>
+          </DropdownMenu.Label>
+          <DropdownMenu.Item className="cursor-pointer">
+            <Link href="/api/auth/signout">Logout</Link>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
   );
 };
 
