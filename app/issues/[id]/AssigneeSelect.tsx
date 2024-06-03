4e1d1ffdf55fa90b224c8e22739105b266aa7eca
uma-rajesh45 <122291360+uma-rajesh45@users.noreply.github.com>
2024-06-03 08:22:33 +0530

    populate the assignee dynamically

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,15 +1,28 @@
 'use client';
+import { User } from '@prisma/client';
 import { Select } from '@radix-ui/themes'
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 const AssigneeSelect = () => {
+  const [users,setUsers] = useState<User []>([]);
+  useEffect(()=>{
+    async function fetchUsers(){
+     const {data} = await axios.get<User []>('/api/users');
+     setUsers(data)
+    }
+    fetchUsers();
+  },[])
   return (
     <Select.Root>
         <Select.Trigger placeholder='Assign...' />
             <Select.Content>
                 <Select.Group>
                     <Select.Label>Suggestions</Select.Label>
-                    <Select.Item value='1'>Uma Rajesh</Select.Item>
+                    {users.map((user)=>(
+                        <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>
+                    ))}
+                    
                 </Select.Group>
             </Select.Content>
        
