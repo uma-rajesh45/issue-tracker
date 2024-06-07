5cd56be520ce76510e75d9dcd56f1645e7321cb7
uma-rajesh45 <122291360+uma-rajesh45@users.noreply.github.com>
2024-06-07 14:58:48 +0530

    implement the pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import Pagination from "./components/Pagination";
 
 const IssuesPage = () => {
-  return <div>IssuesPage</div>;
+  return <Pagination itemCount={100} currentPage={1} pageSize={10}/>;
 };
 
 export default IssuesPage;
